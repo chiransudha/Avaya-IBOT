cf02d0f026aa4bfb13f5d6d2d4b69e88bdfa6ab0
Gitanjali Deshapnde <Gitanjali@avayas-mbp.usaw.avaya.com>
2017-01-18 17:08:09 -0600

    wittoken

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const WIT_TOKEN = ''
+const WIT_TOKEN = 'Q4GGIFA7TFWZ2E5FGDZT7DFOR6FTDHZN'
 if (!WIT_TOKEN) {
   throw new Error('Missing WIT_TOKEN. Go to https://wit.ai/docs/quickstart to get one.')
 }
@@ -14,7 +14,7 @@ if (!FB_PAGE_TOKEN) {
 var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN || 'Sucessfully_compleated_Demo'
 
 module.exports = {
-  WIT_TOKEN: '',
+  WIT_TOKEN: 'Q4GGIFA7TFWZ2E5FGDZT7DFOR6FTDHZN',
   FB_PAGE_TOKEN: 'EAAJgRVVJWfkBAOvpP1CQRXi7gvMBZBM6hqRqOp8634EUr4FtYd0UYewPXglu5HGCC30eAi5nxMv2kDPkauxmO8vVoieAFzo7ieihuEa6RdkDpGNIdtkCskrQkDPUvKbX11UR63BLDvqD2ZCvZBJOTSDlFy7rGNwy3aOkjiMZCHTR8j2w6Voo',
   FB_VERIFY_TOKEN: 'Sucessfully_compleated_Demo',
 }
