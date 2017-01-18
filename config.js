9abce72850e855298b78d9894f7fb802933816c6
Gitanjali Deshapnde <Gitanjali@avayas-mbp.usaw.avaya.com>
2017-01-18 17:45:23 -0600

    invented it

--- config.js
+++ config.js
@@ -6,15 +6,15 @@ if (!WIT_TOKEN) {
 }
 
 
-var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || 'EAAJgRVVJWfkBAOvpP1CQRXi7gvMBZBM6hqRqOp8634EUr4FtYd0UYewPXglu5HGCC30eAi5nxMv2kDPkauxmO8vVoieAFzo7ieihuEa6RdkDpGNIdtkCskrQkDPUvKbX11UR63BLDvqD2ZCvZBJOTSDlFy7rGNwy3aOkjiMZCHTR8j2w6Voo';
+var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || 'EAAJgRVVJWfkBAPbHRLb7jbFkR7h7rvnNZB7762ZCPFzEZBngrbfiUtdY1RvWDlvqus4V7Tl5q8LGG6Ha2oM1ZBiZB3ZBPItVO9nClwjgLZAHDak5WuZC6JAeoMEbuPZBd62r7oqRVnFZCnbIiUXzrfVesoqbD4PopcaxNuLIn6p1FoMDDp4L8VkNsP';
 if (!FB_PAGE_TOKEN) {
 	throw new Error('Missing FB_PAGE_TOKEN. Go to https://developers.facebook.com/docs/pages/access-tokens to get one.')
 }
 
-var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN || 'Sucessfully_compleated_Demo'
+var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN || 'just_do_it'
 
 module.exports = {
   WIT_TOKEN: 'Q4GGIFA7TFWZ2E5FGDZT7DFOR6FTDHZN',
-  FB_PAGE_TOKEN: 'EAAJgRVVJWfkBAOvpP1CQRXi7gvMBZBM6hqRqOp8634EUr4FtYd0UYewPXglu5HGCC30eAi5nxMv2kDPkauxmO8vVoieAFzo7ieihuEa6RdkDpGNIdtkCskrQkDPUvKbX11UR63BLDvqD2ZCvZBJOTSDlFy7rGNwy3aOkjiMZCHTR8j2w6Voo',
-  FB_VERIFY_TOKEN: 'Sucessfully_compleated_Demo',
+  FB_PAGE_TOKEN: 'EAAJgRVVJWfkBAPbHRLb7jbFkR7h7rvnNZB7762ZCPFzEZBngrbfiUtdY1RvWDlvqus4V7Tl5q8LGG6Ha2oM1ZBiZB3ZBPItVO9nClwjgLZAHDak5WuZC6JAeoMEbuPZBd62r7oqRVnFZCnbIiUXzrfVesoqbD4PopcaxNuLIn6p1FoMDDp4L8VkNsP',
+  FB_VERIFY_TOKEN: 'just_do_it',
 }
